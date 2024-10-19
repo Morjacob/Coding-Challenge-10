586bcc51c68a3edc9c7c0c8d25b7fbd8a5301206
Morgan Jacob <jacobmmori@gmail.com>
2024-10-19 14:26:11 -0400

    added "DOMContentLoaded" as a funtion

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
 const priceElement = document.getElementById("product-price");
 const sizeSelector =document.getElementById("size-selector");
 const inventoryCount = document.getElementById("inventory-count");
 const addToPurchaseButton = document.getElementById("purchase-product");
 
-let stock = Number(sizeSelector.options[sizeSelector.selectedIndex].getAttribute("data-stock")); 
-sizeSelector.addEventListener("change" , (event) => {
-    const selectedPrice = event.target.value;
-    priceElement.textContent = `$${selectedPrice}`; 
-    stock = Number(event.target.options[event.target.selectedIndex].getAttribute("data-stock")); 
-    updateInventory();
-})
-
-
-// Task 3- handle stock availability
+let stock = Number(sizeSelector.options[sizeSelector.selectedIndex].getAttribute("data-stock"));
 
 function updateInventory() {
     inventoryCount.textContent =`In Stock: ${stock}`;
     addToPurchaseButton.disabled = stock === 0;
 }
+updateInventory();
+
+sizeSelector.addEventListener("change" , (event) => {
+    const selectedPrice = event.target.value;
+    priceElement.textContent = `$${selectedPrice}`; 
+    stock = Number(event.target.options[event.target.selectedIndex].getAttribute("data-stock")); 
+    updateInventory();
+
+});
 
 addToPurchaseButton.addEventListener("click", () => {
     stock--
     updateInventory();
 })
-
-updateInventory();
+});
