5f01e5efc7d7f45aaaebfa94cf17ba55a7dde782
Morgan Jacob <jacobmmori@gmail.com>
2024-10-19 14:33:16 -0400

    fixed errors

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-const priceElement = document.getElementById("product-price");
-const sizeSelector =document.getElementById("size-selector");
-const inventoryCount = document.getElementById("inventory-count");
-const addToPurchaseButton = document.getElementById("purchase-product");
+    const priceElement = document.getElementById("product-price");
+    const sizeSelector = document.getElementById("size-selector");
+    const inventoryCount = document.getElementById("inventory-count");
+    const addToPurchaseButton = document.getElementById("purchase-product");
 
-let stock = Number(sizeSelector.options[sizeSelector.selectedIndex].getAttribute("data-stock"));
+    let stock = Number(sizeSelector.options[sizeSelector.selectedIndex].getAttribute("data-stock"));
 
-function updateInventory() {
-    inventoryCount.textContent =`In Stock: ${stock}`;
-    addToPurchaseButton.disabled = stock === 0;
-}
-updateInventory();
+    function updateInventory() {
+        inventoryCount.textContent = `In Stock: ${stock}`;
+        addToPurchaseButton.disabled = stock === 0; 
+    }
+    
 
-sizeSelector.addEventListener("change" , (event) => {
-    const selectedPrice = event.target.value;
-    priceElement.textContent = `$${selectedPrice}`; 
-    stock = Number(event.target.options[event.target.selectedIndex].getAttribute("data-stock")); 
     updateInventory();
 
-});
+    sizeSelector.addEventListener("change", (event) => {
+        const selectedPrice = event.target.value;
+        priceElement.textContent = `$${selectedPrice}`; 
+        stock = Number(event.target.options[event.target.selectedIndex].getAttribute("data-stock")); 
+        updateInventory();
+    });
 
-addToPurchaseButton.addEventListener("click", () => {
-    stock--
-    updateInventory();
-})
+    addToPurchaseButton.addEventListener("click", () => {
+        if (stock > 0) { 
+            stock--;
+            updateInventory();
+        }
+    });
 });
