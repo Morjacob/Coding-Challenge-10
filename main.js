a876346be0eed0ad067887f330c4cc6f799de510
Morgan Jacob <jacobmmori@gmail.com>
2024-10-19 16:41:40 -0400

    Fixing errors

--- main.js
+++ main.js
@@ -1,35 +1,28 @@
-
-
-
+let stock = 5
 
     const priceElement = document.getElementById("product-price");
     const sizeSelector = document.getElementById("size-selector");
     const inventoryCount = document.getElementById("inventory-count");
     const addToPurchaseButton = document.getElementById("purchase-product");
 
-    const products = {
-        small: {price: 50, stock: 6},
-        medium: {price: 60, stock:5},
-        large: {price: 70, stock: 7}
-    };
 
     function updateInventory() {
-       const pickedSize = sizeSelector.value;
-       const product = products[pickedSize];
-       priceElement.textContent = `$${product.price}` 
-       inventoryCount.textContent = product.stock > 0? `In stock: ${product.stock}` : "out of stock";
-       addToPurchaseButton.disabled = product.stock <= 0;
-
+        if (stock > 0 ) {
+           addToPurchaseButton.disabled = false;
+           inventoryCount.textContent = `In Stock: ${stock}`; 
+        } else {
+            addToPurchaseButton.disabled = true;
+            inventoryCount.textContent = "Out of Stock"
+        }
     }
-
 addToPurchaseButton.addEventListener("click", () => {
     stock--
     updateInventory();
-})
+}) 
 
     updateInventory();
 
-
+    
 
     sizeSelector.addEventListener("change", (event) => {
         const selectedPrice = event.target.value;
@@ -38,5 +31,4 @@ addToPurchaseButton.addEventListener("click", () => {
     });
 
 
-   
 
